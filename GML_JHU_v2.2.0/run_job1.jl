44ea90361f0acb60d89d3ee34da9263f673dadee
Elijah Pivo <epivo@Elijahs-iMac.local>
2019-09-12 15:14:50 -0400

    ran more predict data

--- GML_JHU_v2.2.0/run_job1.jl
+++ GML_JHU_v2.2.0/run_job1.jl
@@ -15,10 +15,10 @@ SN=6;
 ################################################################################
 
 # Ancillar Markets Considered: "without", "10min", "30min"
-a_types=["without"]
+a_types=["10min", "without"]
 
 # penetation levels
-p_rate = [0.25, 0.5, 0.75, 1];
+p_rate = [0.75, 1]; #[0.25, 0.5, 0.75, 1];
 
 # chance constraint [50, 99] #[95, 90, 85]
 icdf = [0, -2.3263]; #[-1.6449,-1.2816, -1.0364];
@@ -50,7 +50,43 @@ default_i = -1.6449; default_pred_length = 24; default_sem = 0.1;
 
 for a_type in a_types
     for p in p_rate
-        for i in icdf
+        # for i in icdf
+        #     folder = string("./results/");
+        #     filename = "";
+        #     if a_type == "without"
+        #         folder = string(folder, "no_anc/");
+        #         filename = "no_anc_";
+        #     elseif a_type == "10min"
+        #         folder = string(folder, "ten_min_anc/");
+        #         filename = "ten_min_anc_";
+        #     end
+        #     folder = string(folder, "p_rate/p_rate_", Integer(p*100), "/");
+        #     filename = string(filename, "p_rate_", Integer(p*100), "_");
+        #     if i == -1.6449
+        #         folder = string(folder, "cc/cc_95/");
+        #         filename = string(filename, "cc_95");
+        #     elseif i == -1.2816
+        #         folder = string(folder, "cc/cc_90/");
+        #         filename = string(filename,"cc_90");
+        #     elseif i == -1.0364
+        #         folder = string(folder, "cc/cc_85/");
+        #         filename = string(filename,"cc_85");
+        #     elseif i == -2.3263
+        #         folder = string(folder, "cc/cc_99/");
+        #         filename = string(filename,"cc_99");
+        #     elseif i == 0
+        #         folder = string(folder, "cc/cc_50/");
+        #         filename = string(filename,"cc_50");
+        #     end
+        #
+        #     main(a_type, T, BN, F, SN, p, i, default_pred_length, default_sem,
+        #          price_raw, delta_rt_raw, pd_raw, pd_noise, pg_noise, pg_raw,
+        #          folder, filename);
+        #
+        # end
+
+        for pred_length in Pred_length
+
             folder = string("./results/");
             filename = "";
             if a_type == "without"
@@ -62,30 +98,17 @@ for a_type in a_types
             end
             folder = string(folder, "p_rate/p_rate_", Integer(p*100), "/");
             filename = string(filename, "p_rate_", Integer(p*100), "_");
-            if i == -1.6449
-                folder = string(folder, "cc/cc_95/");
-                filename = string(filename, "cc_95");
-            elseif i == -1.2816
-                folder = string(folder, "cc/cc_90/");
-                filename = string(filename,"cc_90");
-            elseif i == -1.0364
-                folder = string(folder, "cc/cc_85/");
-                filename = string(filename,"cc_85");
-            elseif i == -2.3263
-                folder = string(folder, "cc/cc_99/");
-                filename = string(filename,"cc_99");
-            elseif i == 0
-                folder = string(folder, "cc/cc_50/");
-                filename = string(filename,"cc_50");
-            end
 
-            main(a_type, T, BN, F, SN, p, i, default_pred_length, default_sem,
+            folder = string(folder, "pred/pred_", Integer(pred_length*5), "/");
+            filename = string(filename, "pred_", Integer(pred_length*5));
+
+            main(a_type, T, BN, F, SN, p, default_i, pred_length, default_sem,
                  price_raw, delta_rt_raw, pd_raw, pd_noise, pg_noise, pg_raw,
                  folder, filename);
 
         end
 
-        # for pred_length in Pred_length
+        # for sem in solar_error_max
         #
         #     folder = string("./results/");
         #     filename = "";
@@ -99,36 +122,13 @@ for a_type in a_types
         #     folder = string(folder, "p_rate/p_rate_", Integer(p*100), "/");
         #     filename = string(filename, "p_rate_", Integer(p*100), "_");
         #
-        #     folder = string(folder, "pred/pred_", Integer(pred_length*5), "/");
-        #     filename = string(filename, "pred_", Integer(pred_length*5));
+        #     folder = string(folder, "solar/solar_", Integer(sem*1000), "/")
+        #     filename = string(filename, "solar_", Integer(sem*1000));
         #
-        #     main(a_type, T, BN, F, SN, p, default_i, pred_length, default_sem,
-        #          price_raw, delta_rt_raw, pd_raw, pd_noise, pg_noise_25, pg_raw,
+        #     main(a_type, T, BN, F, SN, p, default_i, default_pred_length, sem,
+        #          price_raw, delta_rt_raw, pd_raw, pd_noise, pg_noise, pg_raw,
         #          folder, filename);
         #
         # end
-        #
-        for sem in solar_error_max
-
-            folder = string("./results/");
-            filename = "";
-            if a_type == "without"
-                folder = string(folder, "no_anc/");
-                filename = "no_anc_";
-            elseif a_type == "10min"
-                folder = string(folder, "ten_min_anc/");
-                filename = "ten_min_anc_";
-            end
-            folder = string(folder, "p_rate/p_rate_", Integer(p*100), "/");
-            filename = string(filename, "p_rate_", Integer(p*100), "_");
-
-            folder = string(folder, "solar/solar_", Integer(sem*1000), "/")
-            filename = string(filename, "solar_", Integer(sem*1000));
-
-            main(a_type, T, BN, F, SN, p, default_i, default_pred_length, sem,
-                 price_raw, delta_rt_raw, pd_raw, pd_noise, pg_noise, pg_raw,
-                 folder, filename);
-
-        end
     end
 end
